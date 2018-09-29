b6e1b23425f7a357d6c7e26aafb1c0c2ef708510
Columbus Go Club <columbusgoclub@gmail.com>
2018-09-29 10:09:04 -0400

    organize js

--- scripts/checkin.js
+++ scripts/checkin.js
@@ -35,7 +35,7 @@ const __checkin = (() => {
           /*for each item in the array...*/
           for (i = 0; i < arr.length; i++) {
             /*check if the item starts with the same letters as the text field value:*/
-            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase() ) {
               __checkin.currentFocus = i;
               /*create a DIV element for each matching element:*/
               b = document.createElement("DIV");
